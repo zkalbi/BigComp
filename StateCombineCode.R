c551ab7807e742e9934db760d8b1b247c2801aa2
MartinGuyer <guyerma@onid.oregonstate.edu>
2014-04-11 15:34:29 -0700

    I finished up combining it into 1 Data set, and then I added a new column that has the state to which the observation pertains.

---
+++ StateCombineCode.R
@@ -0,0 +1,20 @@
+#wagp (BW) Wage 
+#schl (BR) [21+ means bachelor or higher] Education
+#rac1p (DU) [9 categories] Race
+#mar (AM) Marital Status
+#esr (CM) [3 means unemployed] Employment Status
+
+BW = 26*2 + 23
+BR = 26*2 + 18
+DU = 26*4 + 21
+AM = 26 + 13
+CM = 26*3 + 13
+BW; BR; DU; AM; CM
+
+State = c(rep("CA", 1097692), rep("OR", 116281), rep("WA", 206451))
+length(State)
+
+setwd("C:/Users/Martin/Documents/BigComp/SimpleData")
+Combined = read.csv("StateCombined.csv", header=TRUE)
+newComb = cbind(Combined, State)
+write.csv(newComb, file="NewStateCombined.csv")
