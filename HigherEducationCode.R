321f1a3b71c3db3f5125f3369d8a787dbc5edaf7
zkalbi <zkalbi@gmail.com>
2014-04-17 10:32:34 -0700

    trying to push

--- HigherEducationCode.R
+++ HigherEducationCode.R
@@ -3,19 +3,19 @@
 #bachelors degree or higher
 
 library(dplyr)
-
+#trying to push
 #Reading in the Data
 newComb = read.csv("NewStateCombined.csv", header=TRUE)
-
+###trying to push
 #Chaning the Data into a tabled dataframe
 CombinedStates_df <- tbl_df(newComb)
-
+test
 #Applying the dplyr commands to find the highest proportions
 CombinedStates_df %.% #Specifying the dataframe
   group_by(State) %.% #Grouping by State
   summarise(HighEd = mean(SCHL > 20, na.rm=TRUE)) %.% #Higher education Proportion
   arrange(desc(HighEd)) #Arranging in Descending order
-
+omit--global
 #We find that Washington Leads with .2405,
 #California second with .22998, and Oregon
 #in last with .22726
